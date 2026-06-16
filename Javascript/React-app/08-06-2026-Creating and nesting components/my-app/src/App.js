8ecc456c766b727a3b790e117aad89105e730e7e
yashprajapati07529 <yashprajapati07529@gmail.com>
2026-06-16 09:46:51 +0530

    react-work

---
+++ Javascript/React-app/08-06-2026-Creating and nesting components/my-app/src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Button from './button';
+
+
+function App() {
+  return (
+    <>
+      <h1>Welcome to React App</h1>
+      <Button />
+    </>
+  );
+}
+
+export default App;
---
+++ Javascript/React-app/08-06-2026-Creating and nesting components/my-app/src/button.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Button = () => {
+    return (
+        <div>
+            <button>click</button>
+            <Button1 />
+        </div>
+    )
+}
+
+export default Button
+
+
+
+const Button1 = () => {
+    return (
+        <div>
+            <button>sumbit</button>
+        </div>
+    )
+}

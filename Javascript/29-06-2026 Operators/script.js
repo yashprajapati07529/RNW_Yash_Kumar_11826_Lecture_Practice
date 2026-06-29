0003818eee1fae0b72c3b5f6a9adb087951def57
yashprajapati07529 <yashprajapati07529@gmail.com>
2026-06-29 11:08:55 +0530

    lab work

---
+++ Javascript/29-06-2026 Operators/script.js
@@ -0,0 +1,82 @@
+// Operators in JavaScript
+
+// Arithmetic Operators
+
+let a = 10;
+let b = 2;
+
+console.log("========= Arithmetic Operators =========");
+
+console.log("Addition:-" + (a + b));
+
+console.log("Subtraction:-" + (a- b));
+
+console.log("Multiplication:-" + (a * b));
+
+console.log("Division:-" + (a / b));
+
+console.log("Modulus:-" + (a % b));
+
+console.log("Exponentiation:-" + (a ** b));
+
+console.log("Pre-Increment:-" + (++a));
+
+console.log("Pre-Decrement:-" + (--b));
+
+console.log("Post-Increment:-" + (a++));
+
+console.log("Post-Decrement:-" + (b--));
+
+// Assignment Operators
+
+console.log("========= Assignment Operators ===========");
+
+let c = 5;
+
+c += 3; 
+
+console.log("Addition Assignment:-" + c);
+
+c -= 4; 
+
+console.log("Subtraction Assignment:-" + c);
+
+c *= 6;
+
+console.log("Multiplication Assignment:-" + c);
+
+c /= 10;
+
+console.log("Division Assignment:-" + c);
+
+c %= 8;
+
+console.log("Modulus Assignment:-" + c);
+
+c **= 7;
+
+console.log("Exponentiation Assignment:-" + c);
+
+// Comparison Operators
+
+console.log("========= Comparison Operators =========");
+
+let x = 20;
+let y = 20;
+
+console.log("Equal to:-" + (x == y));
+
+console.log("Not Equal to:-" + (x != y));
+
+console.log("Strict Equal to:-" + (x === y));
+
+console.log("Strict Not Equal to:-" + (x !== y));
+
+console.log("Greater than:-" + (x > y));
+
+console.log("Less than:-" + (x < y));
+
+console.log("Greater than or Equal to:-" + (x >= y));
+
+console.log("Less than or Equal to:-" + (x <= y));
+
